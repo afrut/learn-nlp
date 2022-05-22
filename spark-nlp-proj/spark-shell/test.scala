fe632f70f0be475e35c072a52cf18fcf3b069105
eric373 <25942990+eric373@users.noreply.github.com>
2022-05-22 17:51:23 -0400

    Use johsnowlabs Explain Document ML pipeline

--- spark-nlp-proj/spark-shell/test.scala
+++ spark-nlp-proj/spark-shell/test.scala
@@ -1,5 +1,42 @@
-import org.apache.spark.sql.types.{StructType, StructField, StringType}
+// :load "spark-shell\\test.scala"
 
+import org.apache.spark.sql.types.{StructType, StructField, StringType, ArrayType}
+import org.apache.spark.sql.functions.{col, split, explode, size}
+
+// Create DataFrame of text files
 val filepath = "..\\data\\mini_newsgroups\\*"
 val texts_df = spark.sparkContext.wholeTextFiles(filepath).toDF("filename", "text")
 texts_df.show()
+
+Create newsgroup column
+val foo = texts_df.withColumn("newsgroup", split($"filename", "/").getItem(6))
+
+import org.apache.spark.ml.PipelineModel
+val explainDocumentPipeline =
+PipelineModel.load("..\\johnsnowlabs.nlp\\explain_document_ml_en_3.0.0_3.0_1616473253101\\")
+
+
+val data = Seq("We are very happy about SparkNLP").map(x => Tuple1(x))
+val df = spark.createDataFrame(data).toDF("text")
+val annotations = explainDocumentPipeline.transform(df)
+
+val sch = annotations.schema
+val cols = sch.map(structfield => structfield.dataType match {
+  case dt: ArrayType => f"${structfield.name}.result"
+  case _ => f"${structfield.name}"
+})
+
+val foo = annotations.select(cols.map(col):_*).
+  toDF(annotations.columns:_*).
+  collect()(0)
+
+val res = scala.collection.mutable.Map[String, List[String]]()
+
+
+
+for (cnt <- 0 to sch.length - 1) {
+  sch(cnt).dataType match {
+    case dt: ArrayType => {res(sch(cnt).name) = foo.getSeq[String](cnt).toList}
+    case _ => {}
+  }
+}
--- spark-nlp-proj/src/main/scala/Main.scala
+++ spark-nlp-proj/src/main/scala/Main.scala
@@ -1,15 +1,18 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.PipelineModel
+
 object Main {
   def main(args: Array[String]){
-    val spark = SparkSession.builder().appName("Test").getOrCreate()
+    // val spark = SparkSession.builder().appName("Test").getOrCreate()
 
-    val filepath = "..\\data\\mini_newsgroups\\"
-    val texts = spark.sparkContext.wholeTextFiles(filepath).toDF(schema = schema)
-    val schema = StructType([StructField("filename", StringType())
-      ,StructType("text", StringType())])
+    // val filepath = "..\\data\\mini_newsgroups\\"
+    // val texts = spark.sparkContext.wholeTextFiles(filepath).toDF(schema = schema)
+    // val schema = StructType([StructField("filename", StringType())
+    //   ,StructType("text", StringType())])
 
-    texts.show()
+    // texts.show()
 
-    spark.stop()
+    val explainDocumentPipeline = PipelineModel.load("..\\johnsnowlabs.nlp\\explain_document_ml_en_3.0.0_3.0_1616473253101\\")
+    // spark.stop()
   }
 }
